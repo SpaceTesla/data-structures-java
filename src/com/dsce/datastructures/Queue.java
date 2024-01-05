49d70d68adc76b9945a2917ea93a30952d74f567
Shashwat <shashwatcse6204@gmail.com>
2024-01-05 19:38:25 +0530

    Queue, deque and priority queue added

---
+++ src/com/dsce/datastructures/Queue.java
@@ -0,0 +1,65 @@
+package com.dsce.datastructures;
+
+public class Queue<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> front;
+    private Node<T> rear;
+    private int size;
+
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (isEmpty()) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T data = front.data;
+        front = front.next;
+        size--;
+        if (isEmpty()) {
+            rear = null; // Reset rear when the last element is dequeued
+        }
+        return data;
+    }
+
+    public void display() {
+        Node<T> temp = front;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
